133f6a0ac1e5e859dd8a7212269630c69abf5244
markustuliy <56708994+markustuliy@users.noreply.github.com>
2023-10-02 23:52:46 +0800

    Initial commit

---
+++ app/src/main/java/com/markustuliy/sexdice/SexDiceActivity.java
@@ -0,0 +1,125 @@
+package com.markustuliy.sexdice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class SexDiceActivity extends AppCompatActivity {
+    private TextView resultBodyPart;
+    private TextView resultAction;
+    private TextView resultStile;
+        private final Random random = new Random();
+
+        @SuppressLint("MissingInflatedId")
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_sex_dice);
+            Button rollButton = findViewById(R.id.rollButton);
+            resultBodyPart = findViewById(R.id.resultBodyPart);
+            resultAction = findViewById(R.id.resultAction);
+            resultStile = findViewById(R.id.resultStile);
+            rollButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Генерируем случайное число от 1 до 10
+                    int diceBodyPart = random.nextInt(7) + 1;
+                    int diceAction = random.nextInt(6) + 1;
+                    int diceStile = random.nextInt(7) + 1;
+
+                    // Отображаем результат на TextView
+                    String diceBodyPartResult = getResultDiceBodyPart(diceBodyPart);
+                    resultBodyPart.setText(diceBodyPartResult);
+                    String diceActionResult = getResultDiceAction(diceAction);
+                    resultAction.setText(diceActionResult);
+                    String diceStileResult = getResultDiceStile(diceStile);
+                    resultStile.setText(diceStileResult);
+                    //resultTextView.setText(String.valueOf(diceBodyPart));
+                }
+            });
+        }
+    private String getResultDiceBodyPart(int number) {
+        String resultText;
+        switch (number) {
+            case 1:
+                resultText = "грудь";
+                break;
+            case 2:
+                resultText = "ягодицы";
+                break;
+            case 3:
+                resultText = "шею";
+                break;
+            case 4:
+                resultText = "губы";
+                break;
+            case 5:
+                resultText = "живот и нижнюю часть живота";
+                break;
+            case 6:
+                resultText = "клитор/пенис";
+                break;
+            default:
+                resultText = "спину";
+                break;
+        }
+        return resultText;
+    }
+    private String getResultDiceAction(int number) {
+        String resultText;
+        switch (number) {
+            case 1:
+                resultText = "массируй";
+                break;
+            case 2:
+                resultText = "покусывай";
+                break;
+            case 3:
+                resultText = "гладь";
+                break;
+            case 4:
+                resultText = "целуй";
+                break;
+            case 5:
+                resultText = "целуй до засоса";
+                break;
+            default:
+                resultText = "облизывай";
+                break;
+        }
+        return resultText;
+    }
+    private String getResultDiceStile(int number) {
+        String resultText;
+        switch (number) {
+            case 1:
+                resultText = "очень ласково";
+                break;
+            case 2:
+                resultText = "страстно";
+                break;
+            case 3:
+                resultText = "медленно";
+                break;
+            case 4:
+                resultText = "быстро";
+                break;
+            case 5:
+                resultText = "глядя прямо в глаза";
+                break;
+            case 6:
+                resultText = "без рук";
+                break;
+            default:
+                resultText = "пенисом/клитором";
+                break;
+        }
+        return resultText;
+    }
+}
